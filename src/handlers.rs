dd9f956b04a634f3b05cb015392a5a947db548b1
James Campbell <james.e.campbell@outlook.com>
2024-05-16 16:56:20 +0100

    Day 3

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,6 +1,47 @@
 use actix_web::{get, HttpResponse};
+use lazy_static::lazy_static;
+use log::info;
+use tera::{Context, Tera};
+
+lazy_static! {
+    pub static ref TEMPLATE: Tera = Tera::new("templates/**/*.html").unwrap();
+}
 
 #[get("/")]
-async fn hello() -> HttpResponse {
-    HttpResponse::Ok().body("Hello World")
+async fn index() -> HttpResponse {
+    match TEMPLATE.render("index.html", &Context::new()) {
+        Ok(rendered) => HttpResponse::Ok().body(rendered),
+        Err(err) => {
+            info!("{}", err);
+            HttpResponse::InternalServerError().body("something went wrong sad face")
+        }
+    }
+}
+
+#[get("/my_first_post")]
+async fn blog_post() -> HttpResponse {
+    let mut context = Context::new();
+    context.insert("title", "My first  post");
+    context.insert("content", "Here is some interesting blog content!");
+    match TEMPLATE.render("post.html", &context) {
+        Ok(rendered) => HttpResponse::Ok().body(rendered),
+        Err(err) => {
+            info!("{}", err);
+            HttpResponse::InternalServerError().body("oh dear it didnt work")
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::{http::StatusCode, test, App};
+
+    #[actix_web::test]
+    async fn index_should_return_ok_response() {
+        let app = test::init_service(App::new().service(index)).await;
+        let req = test::TestRequest::default().to_request();
+        let resp = test::call_service(&app, req).await;
+        assert_eq!(resp.status(), StatusCode::OK);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 mod handlers;
 
+use actix_web::middleware::Logger;
 use actix_web::{App, HttpServer};
-use handlers::hello;
+use handlers::{blog_post, index};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::default())
+            .service(actix_files::Files::new("/static", "static"))
+            .service(index)
+            .service(blog_post)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
